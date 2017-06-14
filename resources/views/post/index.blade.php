7bf17371454f54844ce146f8b02b253a28e8f311
Diego <dcastroaurora@gmail.com>
2017-06-14 01:42:12 -0500

    Primer commit reddt-clone

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row">
+	<div class="col-md-12">
+		<h2>Title</h2>
+		<p>Post r de agosto</p>
+	</div>
+</div>
+<hr>
+<div class="row">
+	<div class="col-md-12">
+		<h2>Title</h2>
+		<p>Post r de agosto</p>
+	</div>
+</div>
+
+@endsection
---
+++ resources/views/post/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row">
+	<div class="col-md-12">
+		<h2>Title</h2>
+		<p>description</p>
+		<p>Post r de agosto</p>
+	</div>
+</div>
+<hr>
+@endsection
